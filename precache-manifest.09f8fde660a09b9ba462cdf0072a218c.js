06ec63e167f6fa06928b5861dcb8304a0d82db2a
RaghavSharma2002 <59282735+RaghavSharma2002@users.noreply.github.com>
2020-06-22 00:13:23 +0530

    Add files via upload

---
+++ precache-manifest.09f8fde660a09b9ba462cdf0072a218c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a1958ee60533cbcfe405da72ff2482",
+    "url": "/E-class-monitor/index.html"
+  },
+  {
+    "revision": "cab3de78ca953ec73d2d",
+    "url": "/E-class-monitor/static/css/main.3cf3463b.chunk.css"
+  },
+  {
+    "revision": "f08cd8c15813a53cb1e5",
+    "url": "/E-class-monitor/static/js/2.1c92b864.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/E-class-monitor/static/js/2.1c92b864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab3de78ca953ec73d2d",
+    "url": "/E-class-monitor/static/js/main.0fb2357b.chunk.js"
+  },
+  {
+    "revision": "0a78e05a21eecfbbd42d",
+    "url": "/E-class-monitor/static/js/runtime-main.84de30f0.js"
+  },
+  {
+    "revision": "b9dcee35494d9c224abf3cba9b7d59f8",
+    "url": "/E-class-monitor/static/media/subjects1.b9dcee35.jpg"
+  },
+  {
+    "revision": "220e2460d46149c29ac68cb010c095f4",
+    "url": "/E-class-monitor/static/media/subjects2.220e2460.png"
+  },
+  {
+    "revision": "8a5f03af8d5ff4057904ea1a671db150",
+    "url": "/E-class-monitor/static/media/subjects3.8a5f03af.jpg"
+  },
+  {
+    "revision": "a99d1cbce8035dbe6e7aaa8834f6abe5",
+    "url": "/E-class-monitor/static/media/subjects4.a99d1cbc.png"
+  },
+  {
+    "revision": "9eb6c5440e94f2a4d0b602f4888d4b97",
+    "url": "/E-class-monitor/static/media/subjects5.9eb6c544.jpg"
+  }
+]);
